125e5ee25abfc29e9bf3225d34a380f94dbc73c5
Matthew Conger-Eldeen <mceldeen@gmail.com>
2018-02-23 16:46:41 -0700

    refactor code so that format is configurable

--- test/Main.purs
+++ test/Main.purs
@@ -7,10 +7,11 @@ import Control.Monad.Eff.AVar (AVAR)
 import Control.Monad.Eff.Console (CONSOLE)
 import Control.Monad.Eff.Random (RANDOM)
 import Test.Unit.Console (TESTOUTPUT)
-import Test.Unit.Main (runTest)
+import Test.Unit.Main (run, runTestWith)
+import Test.Unit.Output.Fancy as Fancy
 
-import Test.Money.USD as Test.Money.USD
 import Test.Money.CanMakeChange as Test.Money.CanMakeChange
+import Test.Money.USD as Test.Money.USD
 
 main ∷ ∀ eff. Eff
   ( console ∷ CONSOLE
@@ -19,6 +20,6 @@ main ∷ ∀ eff. Eff
   , random ∷ RANDOM
   | eff
   ) Unit
-main = runTest do
+main = run $ runTestWith Fancy.runTest do
   Test.Money.USD.main
   Test.Money.CanMakeChange.main
