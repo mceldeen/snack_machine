dffa2d7c8581a4cc8be7ed917c8af5fd35561d7d
Matthew Conger-Eldeen <mceldeen@gmail.com>
2018-02-23 17:22:34 -0700

    add test for failure case

--- src/Money/CanMakeChange.purs
+++ src/Money/CanMakeChange.purs
@@ -3,11 +3,14 @@ module Money.CanMakeChange where
 import Data.Either (Either)
 import Data.Ring (class Ring)
 import Data.Show (class Show)
+import Prelude (class Eq)
 
 data MakeChangeError = CannotMakeChange
 
 instance makeChangeErrorShow ∷ Show MakeChangeError where
     show CannotMakeChange = "CannotMakeChange"
+
+derive instance eqChangeErrorShow ∷ Eq MakeChangeError
 
 class (Ring bag, Ring amount) ⇐ CanMakeChange bag amount where
     -- | Figures out how much to take out of a bag to equal the amount 
--- test/Money/CanMakeChange.purs
+++ test/Money/CanMakeChange.purs
@@ -4,7 +4,7 @@ import Prelude
 
 import Data.Either (Either(..))
 import Data.Lens ((.~), (^.))
-import Money.CanMakeChange (makeChange)
+import Money.CanMakeChange (MakeChangeError(..), makeChange)
 import Money.USD (cents, fiveDollarCount, oneCentCount, oneDollarCount, quarterCount, tenCentCount, twentyDollarCount)
 import Test.Unit (TestSuite, failure, suite, test)
 import Test.Unit.Assert as Assert
@@ -13,17 +13,8 @@ main :: ∀ e. TestSuite e
 main =
   suite "Money.CanMakeChange" do 
     suite "CanMakeChange USDWallet USD" do
-      test "allocates bills from smallest to largest" do
-        case result of
-          Left err → failure $ "unexpected error " <> show err
-          Right change → do
-            Assert.equal 16 (change ^. oneCentCount)
-            Assert.equal 5  (change ^. tenCentCount)
-            Assert.equal 4  (change ^. quarterCount)
-            Assert.equal 3  (change ^. oneDollarCount)
-            Assert.equal 2  (change ^. fiveDollarCount)
-            Assert.equal 1  (change ^. twentyDollarCount)
-        where
+      test "allocates bills from smallest to largest"
+        let
           result = makeChange wallet (cents .~ 3466 $ zero)
 
           wallet = zero # oneCentCount      .~ 26
@@ -32,3 +23,22 @@ main =
                         # oneDollarCount    .~ 3
                         # fiveDollarCount   .~ 2
                         # twentyDollarCount .~ 1
+        in do
+          case result of
+            Left err → failure $ "unexpected error " <> show err
+            Right change → do
+              Assert.equal 16 (change ^. oneCentCount)
+              Assert.equal 5  (change ^. tenCentCount)
+              Assert.equal 4  (change ^. quarterCount)
+              Assert.equal 3  (change ^. oneDollarCount)
+              Assert.equal 2  (change ^. fiveDollarCount)
+              Assert.equal 1  (change ^. twentyDollarCount)
+
+      test "fails when it cannot make exact change"
+        let
+          result = makeChange wallet (zero # cents .~ 1000)
+          wallet = zero # twentyDollarCount .~ 1
+        in do
+          case result of
+            Left err → Assert.equal CannotMakeChange err
+            Right _ → failure "unexpectedly able to make change"
