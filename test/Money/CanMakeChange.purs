92db6cae444282695c0b117fe9241d23a13eaa19
Matthew Conger-Eldeen <mceldeen@gmail.com>
2018-02-23 17:03:34 -0700

    use symbols for lenses

--- test/Money/CanMakeChange.purs
+++ test/Money/CanMakeChange.purs
@@ -3,7 +3,7 @@ module Test.Money.CanMakeChange where
 import Prelude
 
 import Data.Either (Either(..))
-import Data.Lens (set, view)
+import Data.Lens ((.~), (^.))
 import Money.CanMakeChange (makeChange)
 import Money.USD (cents, fiveDollarCount, oneCentCount, oneDollarCount, quarterCount, tenCentCount, twentyDollarCount, usdWallet)
 import Test.Unit (TestSuite, failure, suite, test)
@@ -15,13 +15,13 @@ main =
     suite "CanMakeChange USDWallet USD" do
       test "allocates bills from smallest to largest" do
         let inventory = usdWallet 26 5 4 3 2 1
-        let result = makeChange inventory (set cents 3466 zero)
+        let result = makeChange inventory (cents .~ 3466 $ zero)
         case result of
           Left err → failure $ "unexpected error " <> show err
           Right change → do
-            Assert.equal 16 (view oneCentCount change)
-            Assert.equal 5 (view tenCentCount change)
-            Assert.equal 4 (view quarterCount change)
-            Assert.equal 3 (view oneDollarCount change)
-            Assert.equal 2 (view fiveDollarCount change)
-            Assert.equal 1 (view twentyDollarCount change)
+            Assert.equal 16 (change ^. oneCentCount)
+            Assert.equal 5  (change ^. tenCentCount)
+            Assert.equal 4  (change ^. quarterCount)
+            Assert.equal 3  (change ^. oneDollarCount)
+            Assert.equal 2  (change ^. fiveDollarCount)
+            Assert.equal 1  (change ^. twentyDollarCount)
