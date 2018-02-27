0ce7d2d6867ca7e9e57417e14cad18ca4e06e50c
Matthew Conger-Eldeen <mceldeen@gmail.com>
2018-02-26 18:39:45 -0700

    make the snack machine tests more terse

--- test/SnackMachine.purs
+++ test/SnackMachine.purs
@@ -12,30 +12,18 @@ main =
   suite "SnackMachine" do
     let testSnackMachine = empty ∷ SnackMachine Int Int
     suite "insertMoney" do
-      test "fails when you do not insert a single coin"
-        let
-          invalidCoins = 2
-          insertionResult = insertMoney testSnackMachine invalidCoins
-        in do
-          case insertionResult of
-            Right _ → failure "unexpected success"
-            Left err → Assert.equal CannotInsertMultipleCoinsAtOnce err
+      test "fails when you do not insert a single coin" do
+        case insertMoney testSnackMachine 2 of
+          Right _ → failure "unexpected success"
+          Left err → Assert.equal CannotInsertMultipleCoinsAtOnce err
 
       suite "when you insert a single coin" do
-        test "shows the value in the transaction"
-          let
-            singleCoin = 1
-            insertionResult = insertMoney testSnackMachine singleCoin
-          in do
-            case insertionResult of
-              Left err → failure $ "unexpected error " <> show err
-              Right snackMachine → Assert.equal 1 (coinsInTransaction snackMachine)
+        test "shows the value in the transaction" do
+          case insertMoney testSnackMachine 1 of
+            Left err → failure $ "unexpected error " <> show err
+            Right snackMachine → Assert.equal 1 (coinsInTransaction snackMachine)
 
-        test "shows the value inside the machine"
-          let
-            singleCoin = 1
-            insertionResult = insertMoney testSnackMachine singleCoin
-          in do
-            case insertionResult of
-              Left err → failure $ "unexpected error " <> show err
-              Right snackMachine → Assert.equal 1 (coinsInMachine snackMachine)
+        test "shows the value inside the machine" do
+          case insertMoney testSnackMachine 1 of
+            Left err → failure $ "unexpected error " <> show err
+            Right snackMachine → Assert.equal 1 (coinsInMachine snackMachine)
