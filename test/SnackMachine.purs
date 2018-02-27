1f6282464ee48751486657b20ec0d14b5ed73513
Matthew Conger-Eldeen <mceldeen@gmail.com>
2018-02-26 18:49:06 -0700

    extract out test helpers

--- test/SnackMachine.purs
+++ test/SnackMachine.purs
@@ -4,26 +4,36 @@ import Prelude
 
 import Data.Either (Either(..))
 import SnackMachine (SnackMachine, SnackMachineError(..), coinsInTransaction, coinsInMachine, empty, insertMoney)
-import Test.Unit (TestSuite, failure, suite, test)
+import Test.Unit (TestSuite, Test, failure, suite, test)
 import Test.Unit.Assert as Assert
 
-main :: ∀ e. TestSuite e
+testSnackMachine = empty ∷ SnackMachine Int Int
+
+expectLeft ∷ ∀ v err e. Either err v → (err → Test e) → Test e
+expectLeft r f =
+  case r of
+    Right _ → failure "unexpected success"
+    Left l → f l
+
+expectRight ∷ ∀ v err e. Show err ⇒ Either err v → (v → Test e) → Test e
+expectRight r f =
+  case r of
+    Left err → failure $ "unexpected error " <> show err
+    Right v → f v
+
+main ∷ ∀ e. TestSuite e
 main =
   suite "SnackMachine" do
-    let testSnackMachine = empty ∷ SnackMachine Int Int
     suite "insertMoney" do
       test "fails when you do not insert a single coin" do
-        case insertMoney testSnackMachine 2 of
-          Right _ → failure "unexpected success"
-          Left err → Assert.equal CannotInsertMultipleCoinsAtOnce err
+        expectLeft (insertMoney testSnackMachine 2) \err →
+          Assert.equal CannotInsertMultipleCoinsAtOnce err
 
       suite "when you insert a single coin" do
         test "shows the value in the transaction" do
-          case insertMoney testSnackMachine 1 of
-            Left err → failure $ "unexpected error " <> show err
-            Right snackMachine → Assert.equal 1 (coinsInTransaction snackMachine)
+          expectRight (insertMoney testSnackMachine 1) \snackMachine →
+            Assert.equal 1 (coinsInTransaction snackMachine)
 
         test "shows the value inside the machine" do
-          case insertMoney testSnackMachine 1 of
-            Left err → failure $ "unexpected error " <> show err
-            Right snackMachine → Assert.equal 1 (coinsInMachine snackMachine)
+          expectRight (insertMoney testSnackMachine 1) \snackMachine →
+            Assert.equal 1 (coinsInMachine snackMachine)
